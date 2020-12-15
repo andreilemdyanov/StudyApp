93e741d4e0e8298f46aad36dc7b4be06cab9cb63
andreilemdyanov <lemdyanov5@mail.ru>
2020-12-15 21:09:43 +0300

    CollapsingToolbar.

--- app/src/main/java/ru/fundamentals/studyapp/ui/FragmentMoviesDetails.kt
+++ app/src/main/java/ru/fundamentals/studyapp/ui/FragmentMoviesDetails.kt
@@ -2,27 +2,36 @@ package ru.fundamentals.studyapp.ui
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.TransitionInflater
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.AppBarLayout.OnOffsetChangedListener
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.appbar.MaterialToolbar
 import ru.fundamentals.studyapp.R
 import ru.fundamentals.studyapp.data.Actor
 import ru.fundamentals.studyapp.data.MovieElement
 import ru.fundamentals.studyapp.ui.adapters.ActorsAdapter
 import ru.fundamentals.studyapp.util.loadImage
 import ru.fundamentals.studyapp.util.setRating
+import kotlin.math.abs
 import kotlin.math.roundToInt
 
-class FragmentMoviesDetails : Fragment() {
+class FragmentMoviesDetails : Fragment(R.layout.fragment_movies_details),
+    OnOffsetChangedListener {
 
     private var recycler: RecyclerView? = null
     private var clickListener: ClickListener? = null
+
+    private var tvAge: TextView? = null
+    private var mIsImageHidden = false
+    private var mMaxScrollSize = 0
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -36,34 +45,23 @@ class FragmentMoviesDetails : Fragment() {
         enterTransition = inflater.inflateTransition(R.transition.slide_right)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_movies_details, container, false).apply {
-            findViewById<TextView>(R.id.tv_back).apply {
-                setOnClickListener {
-                    clickListener?.onBackFragmentMoviesListClick()
-                }
-            }
-        }
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         var actors: List<Actor> = emptyList()
-        val tvAge = view.findViewById<TextView>(R.id.tv_minimum_age)
-        val tvTitle = view.findViewById<TextView>(R.id.tv_title_name)
+        tvAge = view.findViewById(R.id.tv_minimum_age)
+        val tvTitle = view.findViewById<CollapsingToolbarLayout>(R.id.collapsing_toolbar)
         val tvGenre = view.findViewById<TextView>(R.id.tv_genre)
         val tvNumRatings = view.findViewById<TextView>(R.id.tv_number_of_ratings)
         val tvOverview = view.findViewById<TextView>(R.id.tv_story_overview)
         val tvCast = view.findViewById<TextView>(R.id.tv_cast)
         val ivBackdrop = view.findViewById<ImageView>(R.id.iv_backdrop)
-
+        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
+        toolbar.setNavigationOnClickListener { clickListener?.onBackFragmentMoviesListClick() }
+        view.findViewById<AppBarLayout>(R.id.app_bar).addOnOffsetChangedListener(this)
         arguments?.apply {
             val movie = getParcelable<MovieElement.Movie>(MOVIE)
-            tvAge.text = getString(R.string.minimum_age, movie?.minimumAge)
-            tvTitle.text = movie?.title
+            tvAge?.text = getString(R.string.minimum_age, movie?.minimumAge)
+            tvTitle.title = movie?.title
             tvGenre.text = movie?.genres?.joinToString { it.name }
             tvNumRatings.text = getString(R.string.number_of_ratings, movie?.numberOfRatings)
             tvOverview.text = movie?.overview
@@ -84,7 +82,6 @@ class FragmentMoviesDetails : Fragment() {
         val adapter = ActorsAdapter(requireContext())
         adapter.submitList(actors)
         recycler?.adapter = adapter
-
     }
 
     override fun onDetach() {
@@ -92,7 +89,29 @@ class FragmentMoviesDetails : Fragment() {
         clickListener = null
     }
 
+    override fun onOffsetChanged(appBarLayout: AppBarLayout?, verticalOffset: Int) {
+        if (mMaxScrollSize == 0) mMaxScrollSize = appBarLayout!!.totalScrollRange
+
+        val currentScrollPercentage: Int = (abs(verticalOffset) * 100
+                / mMaxScrollSize)
+
+        if (currentScrollPercentage >= PERCENTAGE_TO_SHOW_IMAGE) {
+            if (!mIsImageHidden) {
+                mIsImageHidden = true
+                ViewCompat.animate(tvAge!!).scaleY(0f).scaleX(0f).start()
+            }
+        }
+
+        if (currentScrollPercentage < PERCENTAGE_TO_SHOW_IMAGE) {
+            if (mIsImageHidden) {
+                mIsImageHidden = false
+                ViewCompat.animate(tvAge!!).scaleY(1f).scaleX(1f).start()
+            }
+        }
+    }
+
     companion object {
+        private const val PERCENTAGE_TO_SHOW_IMAGE = 10
         private const val MOVIE = "movie"
 
         @JvmStatic
